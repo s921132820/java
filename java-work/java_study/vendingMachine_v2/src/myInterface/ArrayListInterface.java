6c828f016bca6d5672dd1e63c0038e3624ba3858
s921132820 <97858795+s921132820@users.noreply.github.com>
2024-12-12 16:48:41 +0900

    Add files via upload

---
+++ java-work/java_study/vendingMachine_v2/src/myInterface/ArrayListInterface.java
@@ -0,0 +1,26 @@
+package myInterface;
+
+import vo.Product;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ArrayListInterface {
+    public boolean insert(Product product);
+
+    public boolean update(int index, Product product);
+
+    boolean delete(int index);
+
+    Product findByName(String name);
+
+    List<Product> findAll();
+}
+
+
+// 삽입 : boolean inser(Product product) -- 성공하면 true
+// 수정 : boolean update(int index, Product product)
+// 삭제 : boolean delete(int index)
+// 한개 찾아오기(제품명) :
+// Product findByName(String name);
+// 전체 다 찾아오기(전체리스트) :
+// List<Product> findAll();
